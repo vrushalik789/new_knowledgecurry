4acdf24471df29f50527055f1cc277992878c8e6
vrushalik789 <84567896+vrushalik789@users.noreply.github.com>
2021-08-22 17:45:00 +0530

    Add files via upload

---
+++ static.stylemixthemes.com/shared/envato-switcher/inline41a3.js
@@ -0,0 +1,7 @@
+// (function ($) {
+//     $(document).ready(function() {
+//         $.get( "https://static.stylemixthemes.com/shared/envato-switcher/inline.html").done(function( data ) {
+//             $('body').append( data );
+//         });
+//     });
+// })(jQuery);
